ab90222e1a5d53742f28b844646ed595165cac16
Safi <Nurzai.safi@gmail.com>
2022-08-04 09:34:50 +0430

    FEAT: the navbar styling added

--- src/pages/shared/navbar/DesktopNavbar.jsx
+++ src/pages/shared/navbar/DesktopNavbar.jsx
@@ -1,34 +1,98 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
-import '../../../style/navbar.css'
+import "../../../style/navbar.css";
 
 const DesktopNavbar = () => {
-  return <>
-    <div className="navbar-container">
-      <div className="navbar-link">
-        <Link to='/'>Home</Link>
+  const location = useLocation();
+  const pathMatch = (route) => {
+    if (route === location.pathname) {
+      return location;
+    }
+  };
+  return (
+    <>
+      <div
+        className={
+          pathMatch("/") ? "navbar-container" : "navbar-container bg-black"
+        }
+      >
+        <div className="navbar-link">
+          <Link
+            to="/"
+            className={
+              pathMatch("/")
+                ? "font-bold text-primary"
+                : "font-light text-white"
+            }
+          >
+            Home
+          </Link>
+        </div>
+        <div className="navbar-link">
+          <Link
+            to="/about-us"
+            className={
+              pathMatch("/about-us")
+                ? "font-bold text-primary"
+                : "font-light text-white"
+            }
+          >
+            About Us
+          </Link>
+        </div>
+        <div className="navbar-link">
+          <Link
+            to="/production"
+            className={
+              pathMatch("/production")
+                ? "font-bold text-primary "
+                : "font-light text-white"
+            }
+          >
+            Production
+          </Link>
+        </div>
+        <div className="navbar-logo"></div>
+        <div className="navbar-link">
+          <Link
+            to="/programs"
+            className={
+              pathMatch("/programs")
+                ? "font-bold text-primary"
+                : "font-light text-white"
+            }
+          >
+            Programs
+          </Link>
+        </div>
+        <div className="navbar-link">
+          <Link
+            to="/projects"
+            className={
+              pathMatch("/projects")
+                ? "font-bold text-primary"
+                : "font-light text-white"
+            }
+          >
+            Projects
+          </Link>
+        </div>
+        <div className="navbar-link">
+          <Link
+            to="/contact-us"
+            className={
+              pathMatch("/contact-us")
+                ? "font-bold text-primary"
+                : "font-light text-white"
+            }
+          >
+            Contact Us
+          </Link>
+        </div>
       </div>
-      <div className="navbar-link">
-        <Link to='/'>About Us</Link>
-      </div>
-      <div className="navbar-link">
-        <Link to='/'>Production</Link>
-      </div>
-      <div className="navbar-logo">
-
-      </div>
-      <div className="navbar-link">
-        <Link to='/'>Programs</Link>
-      </div>
-      <div className="navbar-link">
-        <Link to='/'>Projects</Link>
-      </div>
-      <div className="navbar-link">
-        <Link to='/'>Contact Us</Link>
-      </div>
-
-    </div>
-  </>;
+    </>
+  );
 };
 
 export default DesktopNavbar;
