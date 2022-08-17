b8edf8bd41837eb7796bd02ef7cc440174d99929
Safi <Nurzai.safi@gmail.com>
2022-08-17 11:11:09 +0430

    FEAT: image styling done

--- src/components/projectsPage/ProjectsPhotoShoot.jsx
+++ src/components/projectsPage/ProjectsPhotoShoot.jsx
@@ -10,23 +10,19 @@ const ProjectsPhotoShoot = () => {
       {
         PhotoCollectionList.map(singlePhoto => {
           return (
-            <li key={singlePhoto.id} className='photo-shoot-list mt-10'>
+            <li key={singlePhoto.id} className='photo-shoot-list mt-10 mb-20'>
               <div className="photo-shoot-caption">
                 <h2 className='text-xl'>{singlePhoto.title}  <span className='text-sm font-extralight'> ({singlePhoto.year})</span></h2>
-                <p className='font-light text-xs mt-4'>{singlePhoto.caption}</p>
-                <h2>Organization: {singlePhoto.org}</h2>
+                <p className='font-light text-xs mt-4 text-grey pb-3 '>{singlePhoto.caption}</p>
+                <h2 className='photo-shoot-list-photos pt-2 pb-4'>Organization: {singlePhoto.org}</h2>
               </div>
-              <div className="photo-shoot-images">
+              <div className="photo-shoot-images flex">
               {
-                // singlePhoto.images.forEach(index => {
-                //   return <p key={index}>Hello shit</p>
-                // })
                 singlePhoto.images.map(singlePhotoImage => {
                   return (
-                    <div key={singlePhotoImage.id} >
-                      <img src={singlePhotoImage.image} />
+                    <div key={singlePhotoImage.id} className='m-2'>
+                      <img src={singlePhotoImage.image} className='h-44'/>
                     </div>
-                    // <p key={index}>Hello shit</p>
                   )
                 }) 
               }
